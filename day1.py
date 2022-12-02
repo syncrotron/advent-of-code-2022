dfe76a4f30790b7511d648735b2bdcbda06008a3
Samuel Horovatin <shorovatin@live.ca>
2022-12-01 20:15:48 -0600

    Day 1 part 2 complete

--- day1.py
+++ day1.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+# Part 1
 max_calories = 0
 calorie_count = 0
+
 for line in open("./input/day1.txt", "r"):
     if line == "\n":
         max_calories = max(max_calories, calorie_count)
@@ -9,4 +11,20 @@ for line in open("./input/day1.txt", "r"):
     else:
         calorie_count = calorie_count + int(line)
 print(f"Max calories: {max_calories}")
+input("Press Enter to continue...")
+
+# Part 2
+max_calories = [0,0,0]
+calorie_count = 0
+calorie_min = 0
+for line in open("./input/day1.txt", "r"):
+    if line == "\n":
+        calorie_min = min(max_calories[0:2])
+        if calorie_count > calorie_min:
+            max_calories.remove(calorie_min)
+            max_calories.append(calorie_count)
+        calorie_count = 0
+    else:
+        calorie_count = calorie_count + int(line)
+print(f"Max calories of top 3 elves: {sum(max_calories)}")
 input("Press Enter to continue...")
