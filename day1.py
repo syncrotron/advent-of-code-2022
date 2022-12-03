d813304b2d39a3fa714ddd87bcce9167f46719a8
Samuel Horovatin <shorovatin@live.ca>
2022-12-02 19:52:22 -0600

    Day 2 part 2: complete

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Samuel Horovatin, December 2022
 
 # Part 1
 max_calories = 0
--- day2.py
+++ day2.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-from re import A
+# Samuel Horovatin, December 2022
 
-MatchScore = {
-    'A' : lambda a: 6 if a == 'Y' else int(a == 'X') * 3, #ROCK
-    'B' : lambda b: 6 if b == 'Z' else int(b == 'Y') * 3, #PAPER
-    'C' : lambda c: 6 if c == 'X' else int(c == 'Z') * 3  #SCISSORS
+MatchScoreInitial = {
+    'A' : lambda a: 6 if a == 'Y' else (a == 'X') * 3, #ROCK
+    'B' : lambda b: 6 if b == 'Z' else (b == 'Y') * 3, #PAPER
+    'C' : lambda c: 6 if c == 'X' else (c == 'Z') * 3  #SCISSORS
 }
     
-HadScore = {
+HadScoreInitial = {
     'X' : 1, #ROCK
     'Y' : 2, #PAPER
     'Z' : 3 #SCISSORS
@@ -15,6 +15,21 @@ HadScore = {
 
 score = 0
 for line in open("./input/day2.txt", "r"):
-    score = score + MatchScore[line[0]](line[2]) + HadScore[line[2]]
+    score = score + MatchScoreInitial[line[0]](line[2]) + HadScoreInitial[line[2]]
 
-print(f"Total Score: {score}")
+print(f"Total Initial Score: {score}")
+input("Press Enter to continue...")
+
+MatchScoreEnhanced = lambda result: 6 if result == 'Z' else (result == 'Y') * 3
+
+HadScoreEnhanced = {
+    'A' : lambda result: 2 if result == 'Z' else (1 if result == 'Y' else 3), # B(2) for Win, A(1) for tie, C(3) for loss
+    'B' : lambda result: 3 if result == 'Z' else (2 if result == 'Y' else 1), # C(3) for Win, B(2) for tie, A(1) for loss
+    'C' : lambda result: 1 if result == 'Z' else (3 if result == 'Y' else 2)  # A(1) for Win, C(3) for tie, B(2) for loss
+}
+
+score = 0
+for line in open("./input/day2.txt", "r"):
+    score = score + MatchScoreEnhanced(line[2]) + HadScoreEnhanced[line[0]](line[2])
+
+print(f"Total Enhanced Score: {score}")
