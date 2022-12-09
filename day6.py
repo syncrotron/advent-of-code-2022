14608434efdb711af6be18b65c7a5bcd48366d3f
syncrotron <shorovatin@live.ca>
2022-12-09 14:22:44 -0600

    Day 6 part 1: Complete

---
+++ day6.py
@@ -0,0 +1,12 @@
+# Samuel Horovatin, December 2022
+
+BUFFER_SIZE = 4
+first_marker = 0
+for line in open("./input/day6.txt", 'r'):
+    for i in range(0, len(line)):
+        max_i = min(i+BUFFER_SIZE, len(line))
+        if len(set(line[i:max_i])) == BUFFER_SIZE:
+             first_marker = i + BUFFER_SIZE
+             break
+
+print(f'First Start-Of-Packet Marker After {first_marker} Chars')
