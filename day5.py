78fd706ad6ccd8c2f29f571df8449ca6b47d6f16
syncrotron <shorovatin@live.ca>
2022-12-09 10:11:31 -0600

    Day 5 part 1: Complete

--- day5.py
+++ day5.py
@@ -1,16 +1,17 @@
 # Samuel Horovatin, December 2022
+import re
 
+# Takes a stack line and extracts 
 def convert_header_line(line, num_of_stacks):
-    edited_line = str(line).replace("] [", ",").replace("    [", "-,").replace("]    ", ",-").replace("[", "").replace("]", "")
-    edited_line = edited_line.replace(" ", ",").replace(",,,", ",-,").replace(",,", ",").strip("\n")
-    return edited_line
+    regex = r'\[[A-Z]\]|    |   \n$' #Regex from Matthew B.
+    edited_line = re.findall(regex, line)
+    return [x.strip("[] \n") for x in edited_line]
 
 def make_move(move_str, stacks):
     moves =  [int(i) for i in str(move_str).strip('\n').split(' ') if i.isdigit()]
     moves = [moves[0], moves[1] - 1, moves[2] - 1]
-    print(len(stacks))
     moved_stack = stacks[moves[1]][0:moves[0]]
-    moved_stack.reverse()
+    moved_stack.reverse() # Needed for single crate pick up and put down
 
     stacks[moves[2]] = moved_stack + stacks[moves[2]]
     stacks[moves[1]] = stacks[moves[1]][moves[0]:]
@@ -19,29 +20,28 @@ def make_move(move_str, stacks):
 num_of_stacks = 0
 raw_levels, corrected_levels, stacks = [], [], []
 for line in open("./input/day5.txt", 'r'):
+
     if 'move' in line:
-        corrected_levels = make_move(line, corrected_levels)
+        stacks = make_move(line, stacks)
     elif ('[' in line) or (']' in line):
         raw_levels.append(line)
     elif not line == "\n":
+        # Must be final line of stack portion of file, indicating number of stacks
         num_of_stacks = int(line.strip('\n').split('   ')[-1])
         for level in raw_levels:
-            corrected_levels.append(convert_header_line(level, num_of_stacks).split(",")[0:num_of_stacks])
-        
-        # Transpose lists
+            corrected_levels.append(convert_header_line(level, num_of_stacks))
+
+        # Transpose stacks order stack tops to be at front of list
         stacks = [list(i) for i in zip(*corrected_levels)]
         for i in range(0, len(stacks)):
             new_stack = []
             for crate in stacks[i]:
-                if not crate == '-':
+                if not crate == '':
                     new_stack.append(crate)
             stacks[i] = new_stack
 
+# Extract top of stack solution string
 top_of_each_stack = []
 for i in range(0,num_of_stacks):
     top_of_each_stack.append(stacks[i][0])
-
-print(top_of_each_stack)
-       
-
-
+print('Top of Stacks Strings:',''.join(top_of_each_stack))  
