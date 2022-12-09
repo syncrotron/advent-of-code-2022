a60254cec9b35e64f53fa45fdd49a7dacb828f37
syncrotron <shorovatin@live.ca>
2022-12-09 14:28:17 -0600

    Day 6 part 2: Complete

--- day6.py
+++ day6.py
@@ -9,4 +9,16 @@ for line in open("./input/day6.txt", 'r'):
              first_marker = i + BUFFER_SIZE
              break
 
-print(f'First Start-Of-Packet Marker After {first_marker} Chars')
+print(f'First Start-Of-Packet Marker After {first_marker} Chars (4 char buffer)')
+input("Press Enter to continue...") 
+
+BUFFER_SIZE = 14
+first_marker = 0
+for line in open("./input/day6.txt", 'r'):
+    for i in range(0, len(line)):
+        max_i = min(i+BUFFER_SIZE, len(line))
+        if len(set(line[i:max_i])) == BUFFER_SIZE:
+             first_marker = i + BUFFER_SIZE
+             break
+
+print(f'First Start-Of-Packet Marker After {first_marker} Chars (14 char buffer)')
