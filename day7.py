d324c55d563f67c2cb40c926880560ed8e2d5a2c
syncrotron <shorovatin@live.ca>
2022-12-14 15:45:38 -0600

    Day 7, part 1: Complete

--- day7.py
+++ day7.py
@@ -6,8 +6,11 @@ from typing import List
 class Node:
     name: str
     size: int
-    parent: 'Node'
+    parent: 'Node' = None
     children: List['Node'] = field(default_factory=list) 
+    
+    def print_node(self):
+        print(f'Name: {self.name}, Size: {self.size}, Parent: {None if self.parent is None else self.parent.name}, # Children: {len(self.children) if self.children is not None else None}')
 
 def find_child(child_name: str, curr_node: Node):
     for child in curr_node.children:
@@ -15,42 +18,53 @@ def find_child(child_name: str, curr_node: Node):
             return child
     return curr_node
 
-def cd(cd_dir: str, curr_node: Node, root_node: Node):
+def find_root(node: Node):
+    while not node.name == '/':
+        node = node.parent
+    return node
+
+def cd(cd_dir: str, curr_node: Node):
     next_node = curr_node 
     if cd_dir == '..' and not curr_node.name == '/':
         next_node = curr_node.parent
     elif cd_dir == '/':
-        next_node = root_node
+        next_node = find_root(next_node)
     elif not curr_node.children == None:
         next_node = find_child(cd_dir, curr_node)
     return next_node
 
-def depthfirst(curr_node: Node, size_dict: dict()):
-    
-    if curr_node.children == None:
-        return size_dict, curr_node.size
+def update_all_sizes(curr_node: Node):
+    if curr_node.children is None:
+        return curr_node, curr_node.size
     else:
         for child in curr_node.children:
-           size_dict, children_size = depthfirst(child, size_dict)
-           curr_node.size = curr_node.size + children_size
-        size_dict[curr_node.name] = curr_node.size
-        return size_dict, curr_node.size
+
+            child, children_size = update_all_sizes(child)
+            curr_node.size = curr_node.size + children_size
+        return curr_node, curr_node.size
+
+def find_size_total_below_thresh(curr_node: Node, total: int, max_size: int):
+    if curr_node.children is None:
+        return total
+    else:
+        total = total + (curr_node.size if curr_node.size <= max_size else 0)
+        for child in curr_node.children:
+            total = find_size_total_below_thresh(child, total, max_size)
+        return total
 
 MAX_DIR_SIZE = 100000
 CD_CMD_LEN = len('$ cd ')
-root_node = Node('/', 0, None)
-curr_node = root_node
+curr_node = Node('/', 0, None)
 file_nodes, dir_nodes = [], []
 for line in open("./input/day7.txt", 'r'):
     line = line.rstrip('\n') # Removes pesky newline
     if '$ cd ' in line:
         cd_dir = line[CD_CMD_LEN:]
-        curr_node = cd(cd_dir, curr_node, root_node) 
-    elif not 'ls' in line:
+        curr_node = cd(cd_dir, curr_node) 
+    elif not '$ ls' in line:
         cmd_parts = line.split(' ')
         # Stops duplicate children
         if not cmd_parts[0] in curr_node.children:
-            
             if cmd_parts[0] == 'dir': 
                 curr_node.children.append(Node(cmd_parts[1], 0, curr_node))
                 dir_nodes.append((cmd_parts[1], 0))
@@ -59,11 +73,7 @@ for line in open("./input/day7.txt", 'r'):
                 curr_node.children.append(new_file_node)
                 file_nodes.append(new_file_node)
 
-root_node = curr_node
-folder_size_dict = dict()
-size_dict, root_size = depthfirst(root_node, folder_size_dict)
+root_node, root_size = update_all_sizes(find_root(curr_node))
 
-dir_sum = 0
-for dir in folder_size_dict:
-    if folder_size_dict[dir] <= MAX_DIR_SIZE:
-        dir_sum = dir_sum + folder_size_dict[dir]
+
+print(find_size_total_below_thresh(root_node, 0, MAX_DIR_SIZE))
