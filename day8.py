953d290580722f23359ad7026ce328ae19e03404
Samuel Horovatin <shorovatin@live.ca>
2022-12-19 17:28:34 -0600

    Day 8 part 1

--- day8.py
+++ day8.py
@@ -4,26 +4,26 @@ visible_trees = 0
 edge_trees = 0
 middle_trees = 0
 tree_grid = []
-for line in open("./input/day8_test.txt", 'r'):
+for line in open("./input/day8.txt", 'r'):
     line = line.rstrip('\n')
     tree_grid.append([int(x) for x in [*line]])
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-for tree_col_i in range(0, len(tree_grid)):
-    for tree_row_i in range(0, len(tree_grid[tree_col_i])):
+for tree_row_i in range(0, len(tree_grid)):
+    for tree_col_i in range(0, len(tree_grid[tree_row_i])):
         if tree_col_i == 0 or tree_row_i == 0 or tree_col_i == (len(tree_grid) - 1) or tree_row_i == (len(tree_grid[tree_col_i]) - 1):       
             edge_trees = edge_trees + 1
         else:
-            selected_tree = tree_grid[tree_col_i][tree_row_i]
-            north_trees = column(tree_grid, tree_col_i)[0:tree_row_i]
-            south_trees = column(tree_grid, tree_col_i)[tree_row_i+1:len(tree_grid)]
-            east_trees = tree_grid[tree_col_i][tree_row_i+1:len(tree_grid)]
-            west_trees = tree_grid[tree_col_i][0:tree_row_i]
-            if selected_tree > max(north_trees) or selected_tree > max(south_trees) or selected_tree > max(east_trees) or selected_tree > max(west_trees):
+            selected_tree = tree_grid[tree_row_i][tree_col_i]
+            top_trees = column(tree_grid, tree_col_i)[0:tree_row_i]
+            bottom_trees = column(tree_grid, tree_col_i)[tree_row_i+1:len(tree_grid)]
+            right_trees = tree_grid[tree_row_i][tree_col_i+1:len(tree_grid)]
+            left_trees = tree_grid[tree_row_i][0:tree_col_i]
+            if selected_tree > max(top_trees) or selected_tree > max(bottom_trees) or selected_tree > max(right_trees) or selected_tree > max(left_trees):
                 middle_trees = middle_trees + 1
         
-print("edge_trees", edge_trees)
-print("middle_trees", middle_trees)
+print("Edge Tree Count:", edge_trees)
+print("Middle Tree Count:", middle_trees)
 print(f"Visibile Trees: {edge_trees + middle_trees}")
